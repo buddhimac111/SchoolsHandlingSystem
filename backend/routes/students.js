3d2ffc4572d48228764e5e431d04b456dda6fdf2
Iddhi <iddhidasanayaka@gmail.com>
2023-03-13 23:19:43 +0530

    Change Student route and delete file uploading with post request

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -1,8 +1,4 @@
 const express = require("express");
-const saveImage = require("../utils/saveImage");
-const _ = require("lodash");
-const fs = require("fs");
-const winston = require("winston");
 const mongoose = require("mongoose");
 
 const { User, validateUser } = require("../models/user");
@@ -30,11 +26,7 @@ router.get("/:id", async (req, res) => {
 
 // create a new student
 router.post("/", async (req, res) => {
-  const picture = req.files ? req.files.picture || undefined : undefined;
-  if (!picture || !/image/.test(picture.mimetype))
-    return res.status(400).send("Picture required or Invalid picture type");
-
-  const { userBody, studentBody } = extractStudent(req.body);
+  const { userBody, studentBody } = req.body;
   const errorUser = validateUser(userBody);
   if (errorUser) return res.status(400).send(errorUser);
 
@@ -56,16 +48,13 @@ router.post("/", async (req, res) => {
     session.startTransaction();
     // add session parameter for each save({session})
     // transactions only supported by replica set
-    user.picture = await saveImage(picture, `profilePic/${user._id}`);
     await user.save();
     await student.save();
-    console.log(user.picture);
+
     res.send(student);
+
     await session.commitTransaction();
   } catch (err) {
-    fs.unlink(`./public/${user.picture}`, (err) => {
-      winston.error(err);
-    });
     res.status(400).send(err.message);
     await session.abortTransaction();
   } finally {
@@ -79,58 +68,23 @@ router.put("/:id", async (req, res) => {
   if (!mongoose.isValidObjectId(id))
     return res.status(400).send("Invalid user id");
 
-  const { userBody, studentBody } = extractStudent(req.body);
+  const { userBody, studentBody } = req.body;
   studentBody.user = id;
+
   const errorUser = validateUser(userBody);
   if (errorUser) return res.status(400).send(errorUser);
+
   const errorStudent = validateStudent(studentBody);
   if (errorStudent) return res.status(400).send(errorStudent);
 
   let result = await User.findByIdAndUpdate(id, userBody, { new: true });
+
   if (!result) return res.status(404).send("User not found");
   result = await Student.findOneAndUpdate({ user: id }, studentBody, {
     new: true,
   });
+
   res.send(result);
 });
-
-// delete a student by their user id
-router.delete("/:id", async (req, res) => {
-  const id = req.params.id;
-  if (!mongoose.isValidObjectId(id))
-    return res.status(400).send("Invalid user id");
-
-  const session = await User.startSession();
-  try {
-    session.startTransaction();
-    const student = await Student.findOneAndRemove({ user: id })
-      .populate("user")
-      .select("-_id -__v");
-    await User.findByIdAndRemove(id);
-    if (!student) return res.status(400).send("Student already deleted");
-    fs.unlink(`./public/${student.user.picture}`, (err) => {
-      winston.error(err);
-    });
-    res.send(student);
-    await session.commitTransaction();
-  } catch (err) {
-    res.send(400).send(err.message);
-    await session.abortTransaction();
-  } finally {
-    await session.endSession();
-  }
-});
-
-function extractStudent(object) {
-  const final = {};
-  final.userBody = _.pick(object, ["name", "email", "password"]);
-  final.studentBody = _.pick(object, ["address", "school", "classe", "DOB"]);
-  final.userBody.role = "student";
-  final.studentBody.parent = {
-    name: object.parentName,
-    phone: object.parentPhone,
-  };
-  return final;
-}
 
 module.exports = router;
