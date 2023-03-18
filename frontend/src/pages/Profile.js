2299cd52be256599e72bdd61c6ab176a5a55dc1d
buddhimac111 <91274532+buddhimac111@users.noreply.github.com>
2023-03-18 10:21:04 +0530

    Created Profile page

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,7 +1,8 @@
 import SideNav from "../components/SideNav";
 import TopBar from "../components/TopBar";
 import Prof from "../assets/proff.png";
-import { FaIdCard, FaAt } from "react-icons/fa";
+import School from "../assets/school.png";
+import { FaIdCard, FaAt, FaUsers } from "react-icons/fa";
 
 const userProfile = () => {
 
@@ -33,10 +34,52 @@ const userProfile = () => {
                     /> <p className="ps-2">das52468@stannes.scl.lk</p>
                   </div>
                   <div class="text-center px-2 py-1 text-white shadow rounded-2 roleTag">Scl. Admin</div>
-
                 </div>
               </div>
               <hr />
+              <div className="row m-0 p-0">
+
+                  <div className="col-lg-4 schoolBox d-flex pt-3 pb-3">
+                    <img className="rounded-circle schoolImage" src={School} alt="schoolImage" />
+                    <div className="schoolTexts">
+                    <h4 className="pt-2 pe-5">St.Anne's College</h4>
+                    <h5>Kurunegala</h5>
+                    </div>
+                    
+                  
+                  </div>
+                  <div className="col classBox pt-4 ps-4">
+                    <h4>Full Name :</h4>
+                    <p className="fw-bold">Don Denister Andrew Samarakoon de Silva</p>
+                    <hr/>
+
+                    <h4>Address :</h4>
+                    <p className="fw-bold">No.45, Mihidu-Mawatha, Dambulla-road, Kurunegala</p>
+                    <hr/>
+
+                    <h4>Started Year :</h4>
+                    <p className="fw-bold">2007</p>
+                    <hr/>
+                    <h4>Started Year :</h4>
+                    <p className="fw-bold">2007</p>
+                    <hr/>
+                  {/* <FaUsers className="pt-4"
+                      size={75}
+                      color={"var(--secondary)"}
+                    /> 
+                    <div className="schoolTexts">
+                    <h4 className="pt-4 pe-4">Grade 13-M2</h4>
+                    </div> */}
+                  
+                  </div>
+
+                 
+                  
+
+              </div>
+
+             
+
 
             </div>
 
