045607a928532a0941e86e6824b77f0f4ba5dac1
Iddhi <iddhidasanayaka@gmail.com>
2023-03-25 23:52:43 +0530

    Added authorization middleware to subjects

--- backend/todo.js
+++ backend/todo.js
@@ -1,2 +1,3 @@
 // TODO: Add increment and decrement for schools when adding teachers and students
 // TODO: Add increment and decrement for classes when adding students
+// TODO: Validate Subject before inserting in timetable and exam results
