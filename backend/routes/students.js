f2a2fe4bd918b038644b0be616677d98a22fbd16
Iddhi <iddhidasanayaka@gmail.com>
2023-03-25 23:45:57 +0530

    Added authorization middle ware to students route

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -4,50 +4,66 @@ const mongoose = require("mongoose");
 const { Student, validateStudent } = require("../models/student");
 const { School } = require("../models/school");
 const { Class } = require("../models/classe");
+const { sAdminAuth, teacherAuth } = require("../middlewares/auth");
 
 const router = express.Router();
 
 // get all the students
-router.get("/", async (req, res) => {
-  const students = await Student.find({}).select("-_id -__v -parent._id");
+router.get("/", sAdminAuth, async (req, res) => {
+  const students = await Student.find({ school: req.user.school }).select(
+    "-_id -__v"
+  );
   res.send(students);
 });
 
-// get student by their user id
-router.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  if (!mongoose.isValidObjectId(id))
-    return res.status(400).send("Invalid user id");
-  const student = await Student.findOne({ user: id })
-    .select("-_id -__v -parent._id")
-    .populate("user");
-  if (!student) return res.status(404).send("User not found");
-  res.send(student);
+// get all the students in a class
+router.get("/class", teacherAuth, async (req, res) => {
+  const students = await Student.find({ classe: req.user.classe }).select(
+    "-_id -__v"
+  );
+  res.send(students);
 });
 
+// TODO: IS this really necesseary?
+// get student by their user id
+// router.get("/:id", async (req, res) => {
+//   const id = req.params.id;
+//   if (!mongoose.isValidObjectId(id))
+//     return res.status(400).send("Invalid user id");
+//   const student = await Student.findOne({ user: id })
+//     .select("-_id -__v")
+//     .populate("user");
+//   if (!student) return res.status(404).send("User not found");
+//   res.send(student);
+// });
+
 // update a student by their user id
-router.put("/:id", async (req, res) => {
+router.put("/:id", sAdminAuth, async (req, res) => {
   const id = req.params.id;
   if (!mongoose.isValidObjectId(id))
     return res.status(400).send("Invalid user id");
 
   req.body.user = id;
+  const student = await Student.findOne({ user: id }).select("-__v");
+  if (!student) return res.status(404).send("User not found");
+  if (student.school.toHexString() !== req.user.school)
+    return res
+      .status(401)
+      .send("Unauthorized access, Student doesent belong to your school");
+
+  req.body.school = student.school.toHexString();
+  req.body.classe = student.classe.toHexString();
 
   const errorStudent = validateStudent(req.body);
   if (errorStudent) return res.status(400).send(errorStudent);
 
-  const school = await School.findById(req.body.school);
-  if (!school) return res.status(400).send("School not found");
+  student.parent = req.body.parent;
+  student.address = req.body.address;
+  student.DOB = req.body.DOB;
 
-  const classe = await Class.findById(req.body.classe);
-  if (!classe) return res.status(400).send("Class not found");
+  await student.save();
 
-  const result = await Student.findOneAndUpdate({ user: id }, req.body, {
-    new: true,
-  });
-  if (!result) return res.status(404).send("User not found");
-
-  res.send(result);
+  res.send(student);
 });
 
 module.exports = router;
